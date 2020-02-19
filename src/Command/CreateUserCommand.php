2e5cbfea0183a738debd3273e90499ba9e597ad8
Denisn Savin <sawi.denis@yandex.ru>
2020-02-19 20:08:40 +0300

    works a little

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -1,16 +1,57 @@
 <?php
-
+declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Entity\Uploader;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
 use Symfony\Component\Console\Input\InputOption;
-class CreateUserCommand extends Command
+
+class TIMER {
+
+    var $T1 = null;
+    var $T2 = null;
+
+    function __construct() {
+        $this->start();
+    }
+
+    //запустили секундомер
+    public function start() {
+        $this->T1 = microtime(TRUE);
+        $this->T2 = null;
+    }
+
+    //остановили секундомер
+    public function stop() {
+        $this->T2 = microtime(TRUE);
+    }
+
+    //измерить результат в секундах (вплоть до мкс)
+    public function result() {
+        //финишная отметка не определена
+        if (is_null($this->T2)) {
+            $this->stop();
+        }
+        return $this->T2 - $this->T1;
+    }
+}
+
+class CreateUserCommand extends ContainerAwareCommand
 {
+
+
     protected static $defaultName = 'app:import';
+
     protected function configure()
     {
 //        $this->addArgument('filePath', InputArgument::REQUIRED, 'Path to CSV file');
@@ -22,14 +63,38 @@ class CreateUserCommand extends Command
             'dataset.csv'
         );
     }
-
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $text = 'Path - '.$input->getOption('filePath');
+//        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager = $this->getContainer()->get('doctrine')->getManager();
 
+        $T = new TIMER();
+        $file = fopen($input->getOption('filePath'), "r");
+        $i=1;
+        while (($data = fgetcsv($file, 300000, " ")) !== FALSE)
+        {
+            $uploader = new Uploader();
+            $uploader->setTimestamp($data[0]);
+            $uploader->setRfc($data[1]);
+            $uploader->setDomain($data[2]);
+            $uploader->setSize($data[3]);
+            $uploader->setPath($data[4]);
+            $uploader->setAgent($data[5]);
+            $uploader->setStatus($data[6]);
+            $uploader->setMethod($data[7]);
+            $uploader->setType($data[8]);
+            $output->writeln($i);
+            $entityManager->persist($uploader);
+            if (memory_get_usage() > 30217728){
+                $entityManager->flush();
+                unset($uploader);
+            }
+            $i++;
+        }
 
-        $output->writeln($text.'!');
+        fclose($file);
+
+        $output->writeln("Время выполнения кода: " . $T->result() . " c.");
 
         return 0;
     }
---
+++ src/Entity/Uploader.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UploaderRepository")
+ */
+class Uploader
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $timestamp;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $rfc;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $domain;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $size;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $agent;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $method;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTimestamp(): ?string
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(string $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    public function getRfc(): ?string
+    {
+        return $this->rfc;
+    }
+
+    public function setRfc(string $rfc): self
+    {
+        $this->rfc = $rfc;
+
+        return $this;
+    }
+
+    public function getDomain(): ?string
+    {
+        return $this->domain;
+    }
+
+    public function setDomain(string $domain): self
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    public function getSize(): ?string
+    {
+        return $this->size;
+    }
+
+    public function setSize(string $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getAgent(): ?string
+    {
+        return $this->agent;
+    }
+
+    public function setAgent(string $agent): self
+    {
+        $this->agent = $agent;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getMethod(): ?string
+    {
+        return $this->method;
+    }
+
+    public function setMethod(string $method): self
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
